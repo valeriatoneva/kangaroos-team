99f8083a2add4654664db4d38eda44de328f2af5
ValeriaHoody <valeria@hoody.email>
2023-08-23 11:41:44 +0300

    feat: adding assets and other useful files

--- client/kangaroos-team-frontend/src/components/AppNavbar/Sidebar.js
+++ client/kangaroos-team-frontend/src/components/AppNavbar/Sidebar.js
@@ -1,16 +1,16 @@
 import React from 'react';
-import { Link } from 'react-router-dom'; // Import the Link component
+import { Link } from 'react-router-dom';
 import './Sidebar.css';
 
 function Sidebar() {
   return (
     <div className="sidebar">
-      <div className="logo">Kangaroos Team</div>
+      <div className="logo">Your App Name</div>
       <ul className="nav-links">
-        {/* Use the Link component to create links */}
         <li><Link to="/">Home</Link></li>
         <li><Link to="/calendar">Calendar</Link></li>
         <li><Link to="/history">Car History</Link></li>
+        {/* Add more navigation links here */}
       </ul>
     </div>
   );
--- client/kangaroos-team-frontend/src/components/Calendar/Calendar.js
+++ client/kangaroos-team-frontend/src/components/Calendar/Calendar.js
@@ -1,43 +1,28 @@
 import React from 'react';
 import { Calendar as BigCalendar, momentLocalizer } from 'react-big-calendar';
 import moment from 'moment';
-import './Calendar.css';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
+import './Calendar.css';
 
 const localizer = momentLocalizer(moment);
 
 function Calendar() {
   const events = [
-    {
-      start: moment().toDate(),
-      end: moment().add(1, 'days').toDate(),
-      title: 'Sample Event',
-    },
-    // ... add more sample events as desired
+    // Define your events here
+    // Each event should have properties like title, start, end, etc.
   ];
 
   return (
     <div className="calendar-container">
       <BigCalendar
         localizer={localizer}
-        defaultDate={new Date()}
-        defaultView="month"
         events={events}
-        style={{ flex: 1 }}
+        startAccessor="start"
+        endAccessor="end"
+        style={{ height: 600 }}
       />
-
-      {/* Integrated HTML */}
-      <div className="e1_4332">
-        <div className="e101_2449"></div>
-        {/* ... Your provided HTML ... */}
-      </div>
-
-      <div className="e3_1488">
-        <div className="e3_1493"></div>
-      </div>
     </div>
   );
 }
 
 export default Calendar;
-
